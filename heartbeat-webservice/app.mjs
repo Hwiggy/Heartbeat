46dafa5cecf73f251b7065539b6c7bfebbeacbe6
Hunter Wignall <hwignall2000@gmail.com>
2023-03-29 19:28:41 -0400

    Javascript is so frustrating x2

--- heartbeat-webservice/app.mjs
+++ heartbeat-webservice/app.mjs
@@ -3,19 +3,7 @@ import * as WebSocket from 'ws'
 
 import config from './config.json' assert { type: "json" }
 const app = express()
-const server = app.listen(config.port, config.host, () => {
-    console.log("Heartbeat webservice is now online")
-})
-const wss = new WebSocket.WebSocketServer({ server })
 
-wss.on('connection', ws => {
-    ws.on('error', console.error)
-    ws.send("Welcome to WS")
-    ws.on('message', data => {
-        console.log(`received: ${data}`)
-        ws.send(`replying for ${data}`)
-    })
-})
 
 import passport from 'passport';
 import Strategy from 'passport-discord';
@@ -31,4 +19,18 @@ app.get('/auth/callback', passport.authenticate("discord", {
     failureRedirect: "/"
 }), (req, res) => {
 
+})
+
+const server = app.listen(config.port, config.host, () => {
+    console.log("Heartbeat webservice is now online")
+})
+const wss = new WebSocket.WebSocketServer({ server })
+
+wss.on('connection', ws => {
+    ws.on('error', console.error)
+    ws.send("Welcome to WS")
+    ws.on('message', data => {
+        console.log(`received: ${data}`)
+        ws.send(`replying for ${data}`)
+    })
 })
